be32220c18c887641938dae2016941c91d6f250a
Scorpion-Ceeb <sebastiansuarezgomez01@gmail.com>
2022-07-07 01:16:43 -0400

    Ready UglyBlazor

--- BlazorApp/Classes/MakingGames.cs
+++ BlazorApp/Classes/MakingGames.cs
@@ -4,36 +4,20 @@ using Domino;
 public static class MakingGame
 {
     
-    public static GameObject[] MakingGames(Value values, int times)
+    public static GameObject[] MakingGames(Value values, int amountPlays)
     {
-        GameObject[] games = new GameObject[times];
+        GameObject[] games = new GameObject[amountPlays];
 
         for (int i = 0; i < games.Length; i++)
         {
             Settings changes = new Settings(values.GetBoard()!, values.GetWinnable()!, values.GetRounder()!, 
                                                     values.GetShuffler()!, values.GetHandCounter()!);
-            games[i] = new GameObject(values.GetMaxHandSize(), MakingGame.MakingPlayers(values.Players!, values.GetAmountPlayers()), changes, values.GetMaximumSize());
+            games[i] = new GameObject(values.GetMaxHandSize(), 
+                                    values.Players!, 
+                                    changes, 
+                                    values.GetMaximumSize());
         }
 
         return games;
-    }
-    public static IPlayer[] MakingPlayers(string[] players, int amount){
-        string[] playersInstances = Utils.GetPlayers();
-        List<IPlayer> listPlayers= new List<IPlayer>();
-        for (int i = 0; i < players.Length; i++)
-        {
-            listPlayers.Add(Value.GetPlayer(players[i])!);
-            amount--;
-        }
-        int count = 0;
-        while (amount-- > 0)
-        {
-            if(players.Contains(playersInstances[count]))
-            {
-                listPlayers.Add(Value.GetPlayer(players[count])!);
-            }
-            count++;
-        }
-        return listPlayers.ToArray();
     }
 }
--- BlazorApp/Classes/Values.cs
+++ BlazorApp/Classes/Values.cs
@@ -3,19 +3,27 @@ using Domino;
 public class Value
 {
     public string? Winnable { get; set; } = Utils.GetWinnables().FirstOrDefault();
-    public string? Rounder { get; set; } = Utils.GetRounders().FirstOrDefault();
+    public string? Rounder { get; set; } = Utils.GetRounders().FirstOrDefault(); 
     public string? Shuffler{ get; set;} = Utils.GetShufflers().FirstOrDefault();
     public string? HandCounter{ get; set;} = Utils.GetHandCounters().FirstOrDefault();
     public string? Board{ get; set;} = Utils.GetBoards().FirstOrDefault();
-    public string[]? Players{ get; set;}
+    public IPlayer[]? Players{get;set;}
     public string Amount{ get; set;}="1";
     public string MaximumSize{get;set;}="9";
     public string MaxHandSize{get;set;}="10";
     public string AmountPlayers{get;set;}="4";
+    public string? Player1Mode{ get; set; } = Utils.GetPlayers().FirstOrDefault();
+    public string? Player1Name{ get; set; }
+    public string? Player2Mode{ get; set; } = Utils.GetPlayers().FirstOrDefault();
+    public string? Player2Name{ get; set; }
+    public string? Player3Mode{ get; set; } = Utils.GetPlayers().FirstOrDefault();
+    public string? Player3Name{ get; set; }
+    public string? Player4Mode{ get; set; } = Utils.GetPlayers().FirstOrDefault();
+    public string? Player4Name{ get; set; }
 
 
     public Value(){}
-    public Value(string winnable, string rounder, string shuffler, string handCounter, string board, string amount, string maximumSize, string maxHandSize, string amountPlayers, string[] players)
+    public Value(string winnable, string rounder, string shuffler, string handCounter, string board, string amount, string maximumSize, string maxHandSize, string amountPlayers, IPlayer[] players)
     {
         this.Winnable = winnable;
         this.Rounder = rounder;
@@ -24,7 +32,7 @@ public class Value
         this.Board= board;
         this.Amount = amount;
         this.MaximumSize = maximumSize;
-        this.MaxHandSize =maxHandSize;
+        this.MaxHandSize = maxHandSize;
         this.AmountPlayers = amountPlayers;
         this.Players = players;
     }
@@ -54,10 +62,23 @@ public class Value
         
         return Activator.CreateInstance(t!) as Board;
     }
-    public static IPlayer? GetPlayer(string player)
+    public IPlayer? GetPlayer(string player, string name)
     {
-        Type? t  = typeof(IPlayer).Assembly.GetTypes().FirstOrDefault(p => p.Name == player);
-        return Activator.CreateInstance(t!) as IPlayer;
+        Type? t = typeof(IPlayer).Assembly.GetTypes().FirstOrDefault(p => p.Name == player);
+        return Activator.CreateInstance(t!, name) as IPlayer;
+    }
+    public IPlayer[] GetPlayers(int amount)
+    {
+        IPlayer[] source = new IPlayer[]{GetPlayer(Player1Mode!, Player1Name!)!, 
+                                        GetPlayer(Player2Mode!, Player2Name!)!, 
+                                        GetPlayer(Player3Mode!, Player3Name!)!, 
+                                        GetPlayer(Player4Mode!, Player4Name!)!};
+
+        IPlayer[] players = new IPlayer[amount];
+
+        Array.Copy(source, players, players.Length);
+
+        return players;
     }
     public int GetAmount()
     {
